e5218d5de6ac2ff2d3b219b877ed83457180f571
Alexander Efremenkov <implimentz@gmail.com>
2016-12-13 11:09:18 +0300

    some javadoc fixes

--- library/src/main/java/com/ironz/binaryprefs/cache/CacheAdapter.java
+++ library/src/main/java/com/ironz/binaryprefs/cache/CacheAdapter.java
@@ -10,57 +10,57 @@ import java.util.Set;
 // TODO: 12/12/16 check SharedPreferences allows null or not
 public interface CacheAdapter {
     /**
-     * Adds {@link java.lang.String} value by specified key.
-     * If cache implementation already has the value by this key it will be overwritten.
+     * Adds {@link java.lang.String} defValue by specified key.
+     * If cache implementation already has the defValue by this key it will be overwritten.
      *
-     * @param key   unique key
-     * @param value value
+     * @param key      unique key
+     * @param defValue default value
      */
-    void putString(String key, String value);
+    void putString(String key, String defValue);
 
     /**
      * Adds {@link java.util.Set} of {@link java.lang.String} values by specified key.
-     * If cache implementation already has the value by this key it will be overwritten.
+     * If cache implementation already has the defValue by this key it will be overwritten.
      *
-     * @param key   unique key
-     * @param value value
+     * @param key      unique key
+     * @param defValue def value
      */
     // TODO: 12/12/16 implement a few designs for storing a multiple values into one file table
-    void putStringSet(String key, Set<String> value);
+    void putStringSet(String key, Set<String> defValue);
 
     /**
-     * Adds {@link java.lang.Integer} primitive value by specified key.
-     * If cache implementation already has the value by this key it will be overwritten.
+     * Adds {@link java.lang.Integer} primitive defValue by specified key.
+     * If cache implementation already has the defValue by this key it will be overwritten.
      *
-     * @param key   unique key
-     * @param value value
+     * @param key      unique key
+     * @param defValue def value
      */
-    void putInt(String key, int value);
+    void putInt(String key, int defValue);
 
     /**
-     * Adds {@link java.lang.Long} primitive value by specified key.
-     * If cache implementation already has the value by this key it will be overwritten.
+     * Adds {@link java.lang.Long} primitive defValue by specified key.
+     * If cache implementation already has the defValue by this key it will be overwritten.
      *
-     * @param key   unique key
-     * @param value value
+     * @param key      unique key
+     * @param defValue def value
      */
-    void putLong(String key, long value);
+    void putLong(String key, long defValue);
 
     /**
-     * Adds {@link java.lang.Float} primitive value by specified key.
-     * If cache implementation already has the value by this key it will be overwritten.
+     * Adds {@link java.lang.Float} primitive defValue by specified key.
+     * If cache implementation already has the defValue by this key it will be overwritten.
      *
-     * @param key   unique key
-     * @param value value
+     * @param key      unique key
+     * @param defValue def value
      */
-    void putFloat(String key, float value);
+    void putFloat(String key, float defValue);
 
     /**
-     * Adds {@link java.lang.Boolean} primitive value by specified key.
-     * If cache implementation already has the value by this key it will be overwritten.
+     * Adds {@link java.lang.Boolean} primitive defValue by specified key.
+     * If cache implementation already has the defValue by this key it will be overwritten.
      *
-     * @param key   unique key
-     * @param value value
+     * @param key      unique key
+     * @param defValue def value
      */
-    void putBoolean(String key, boolean value);
+    void putBoolean(String key, boolean defValue);
 }
