63e43fd23b9f16e9fe48297d81fa56af54ee7cbb
Alexander Efremenkov <implimentz@gmail.com>
2016-12-14 20:24:57 +0300

    ConcurrentCacheAdapterImpl.java - separate cache instances are removed due to class cast public api behavior

--- library/src/main/java/com/ironz/binaryprefs/cache/ConcurrentCacheAdapterImpl.java
+++ library/src/main/java/com/ironz/binaryprefs/cache/ConcurrentCacheAdapterImpl.java
@@ -8,91 +8,85 @@ import java.util.concurrent.ConcurrentHashMap;
  * Concrete cache adapter which implements concurrent cache operations
  */
 // TODO: 13/12/16 implement tests
+@SuppressWarnings("unchecked")
 public class ConcurrentCacheAdapterImpl implements CacheAdapter {
 
-    //we are define explicit type because invokevirtual faster than invokeinterface
-    //also we are define separate caches instead of <String, Object> because class boxing/unboxing faster than class type cast
-    private final ConcurrentHashMap<String, String> stringCache = new ConcurrentHashMap<>();
-    private final ConcurrentHashMap<String, Set<String>> stringSetCache = new ConcurrentHashMap<>();
-    private final ConcurrentHashMap<String, Integer> intCache = new ConcurrentHashMap<>();
-    private final ConcurrentHashMap<String, Long> longCache = new ConcurrentHashMap<>();
-    private final ConcurrentHashMap<String, Float> floatCache = new ConcurrentHashMap<>();
-    private final ConcurrentHashMap<String, Boolean> booleanCache = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, Object> cache = new ConcurrentHashMap<>();
 
     @Override
     public void putString(String key, String value) {
-        stringCache.put(key, value);
+        cache.put(key, value);
     }
 
     @Override
     public void putStringSet(String key, Set<String> values) {
-        stringSetCache.put(key, values);
+        cache.put(key, values);
     }
 
     @Override
     public void putInt(String key, int value) {
-        intCache.put(key, value);
+        cache.put(key, value);
     }
 
     @Override
     public void putLong(String key, long value) {
-        longCache.put(key, value);
+        cache.put(key, value);
     }
 
     @Override
     public void putFloat(String key, float value) {
-        floatCache.put(key, value);
+        cache.put(key, value);
     }
 
     @Override
     public void putBoolean(String key, boolean value) {
-        booleanCache.put(key, value);
+        cache.put(key, value);
     }
 
     @Override
     public String getString(String key, String defValue) {
-        if (stringCache.containsKey(key)) {
-            return stringCache.get(key);
+        if (cache.containsKey(key)) {
+            return (String) cache.get(key);
         }
         return defValue;
     }
 
     @Override
     public Set<String> getStringSet(String key, Set<String> defValues) {
-        if (stringSetCache.containsKey(key)) {
-            return stringSetCache.get(key);
+        if (cache.containsKey(key)) {
+            return (Set<String>) cache.get(key);
         }
         return defValues;
     }
 
     @Override
     public int getInt(String key, int defValue) {
-        if (intCache.containsKey(key)) {
-            return intCache.get(key);
+        if (cache.containsKey(key)) {
+            return (int) cache.get(key);
         }
         return defValue;
     }
 
     @Override
     public long getLong(String key, long defValue) {
-        if (longCache.containsKey(key)) {
-            return longCache.get(key);
+        if (cache.containsKey(key)) {
+            return (long) cache.get(key);
         }
         return defValue;
     }
 
     @Override
     public float getFloat(String key, float defValue) {
-        if (floatCache.containsKey(key)) {
-            return floatCache.get(key);
+        if (cache.containsKey(key)) {
+            return (float) cache.get(key);
         }
         return defValue;
     }
 
     @Override
     public boolean getBoolean(String key, boolean defValue) {
-        if (booleanCache.containsKey(key)) {
-            return booleanCache.get(key);
+        if (cache.containsKey(key)) {
+            return (boolean) cache.get(key);
         }
         return defValue;
     }
