d608f1c706ab7441fc6e07048ae0d04b25ba7198
Alexander Efremenkov <implimentz@gmail.com>
2016-12-16 13:03:37 +0300

    BinaryPreferences.java, BinaryPreferencesEditor.java - added base public api entities

---
+++ library/src/main/java/com/ironz/binaryprefs/BinaryPreferences.java
@@ -0,0 +1,64 @@
+package com.ironz.binaryprefs;
+
+import android.content.SharedPreferences;
+
+import java.util.Map;
+import java.util.Set;
+
+public class BinaryPreferences implements SharedPreferences {
+
+    @Override
+    public Map<String, ?> getAll() {
+        return null;
+    }
+
+    @Override
+    public String getString(String key, String defValue) {
+        return null;
+    }
+
+    @Override
+    public Set<String> getStringSet(String key, Set<String> defValues) {
+        return null;
+    }
+
+    @Override
+    public int getInt(String key, int defValue) {
+        return 0;
+    }
+
+    @Override
+    public long getLong(String key, long defValue) {
+        return 0;
+    }
+
+    @Override
+    public float getFloat(String key, float defValue) {
+        return 0;
+    }
+
+    @Override
+    public boolean getBoolean(String key, boolean defValue) {
+        return false;
+    }
+
+    @Override
+    public boolean contains(String key) {
+        return false;
+    }
+
+    @Override
+    public Editor edit() {
+        return null;
+    }
+
+    @Override
+    public void registerOnSharedPreferenceChangeListener(OnSharedPreferenceChangeListener listener) {
+
+    }
+
+    @Override
+    public void unregisterOnSharedPreferenceChangeListener(OnSharedPreferenceChangeListener listener) {
+
+    }
+}
---
+++ library/src/main/java/com/ironz/binaryprefs/BinaryPreferencesEditor.java
@@ -0,0 +1,64 @@
+package com.ironz.binaryprefs;
+
+import android.content.SharedPreferences;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class BinaryPreferencesEditor implements SharedPreferences.Editor {
+
+    private final Map<String, Object> commitMap = new HashMap<>();
+
+
+
+    @Override
+    public SharedPreferences.Editor putString(String key, String value) {
+        return null;
+    }
+
+    @Override
+    public SharedPreferences.Editor putStringSet(String key, Set<String> values) {
+        return null;
+    }
+
+    @Override
+    public SharedPreferences.Editor putInt(String key, int value) {
+        return null;
+    }
+
+    @Override
+    public SharedPreferences.Editor putLong(String key, long value) {
+        return null;
+    }
+
+    @Override
+    public SharedPreferences.Editor putFloat(String key, float value) {
+        return null;
+    }
+
+    @Override
+    public SharedPreferences.Editor putBoolean(String key, boolean value) {
+        return null;
+    }
+
+    @Override
+    public SharedPreferences.Editor remove(String key) {
+        return null;
+    }
+
+    @Override
+    public SharedPreferences.Editor clear() {
+        return null;
+    }
+
+    @Override
+    public boolean commit() {
+        return false;
+    }
+
+    @Override
+    public void apply() {
+
+    }
+}
