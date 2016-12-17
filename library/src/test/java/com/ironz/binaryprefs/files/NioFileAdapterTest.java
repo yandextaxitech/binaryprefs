0cc5f41229de540f45ef42a7deee5daa7aef92f1
Alexander Efremenkov <implimentz@gmail.com>
2016-12-17 12:42:45 +0300

    NioFileAdapterTest.java - added tests for file ops

--- library/src/test/java/com/ironz/binaryprefs/files/NioFileAdapterTest.java
+++ library/src/test/java/com/ironz/binaryprefs/files/NioFileAdapterTest.java
@@ -8,11 +8,14 @@ import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
 
+import static org.junit.Assert.assertEquals;
+
 public class NioFileAdapterTest {
+
+    private final String name = "file.name";
 
     @Rule
     public TemporaryFolder folder = new TemporaryFolder();
-
     private FileAdapter fileAdapter;
 
     @Before
@@ -22,13 +25,26 @@ public class NioFileAdapterTest {
     }
 
     @Test(expected = FileOperationException.class)
-    public void incorrectDirectory() {
+    public void deletedDirectory() {
         folder.delete(); //trying to remove root directory before starting write operation
-        fileAdapter.save("file.name", "value".getBytes());
+        fileAdapter.save(name, "value".getBytes());
     }
 
     @Test
     public void correctSaving() {
-        fileAdapter.save("file.name", "value".getBytes());
+        fileAdapter.save(name, "value".getBytes());
+    }
+
+    @Test(expected = FileOperationException.class)
+    public void missingFile() {
+        fileAdapter.fetch(name);
+    }
+
+    @Test
+    public void correctFile() {
+        byte[] bytes = "value".getBytes();
+        fileAdapter.save(name, bytes);
+        byte[] fetch = fileAdapter.fetch(name);
+        assertEquals(new String(bytes), new String(fetch));
     }
 }
