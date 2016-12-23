d486f3ec264e918e29451e3397e076fd5fbf6889
Alexander Efremenkov <implimentz@gmail.com>
2016-12-23 20:29:33 +0300

    javadoc

--- library/src/main/java/com/ironz/binaryprefs/exception/FileOperationException.java
+++ library/src/main/java/com/ironz/binaryprefs/exception/FileOperationException.java
@@ -1,6 +1,14 @@
 package com.ironz.binaryprefs.exception;
 
+
+/**
+ * Throws if I/O file operations not finished appropriately
+ */
 public class FileOperationException extends RuntimeException {
+
+    /**
+     * @param cause base cause
+     */
     public FileOperationException(Throwable cause) {
         super(cause);
     }
