346c8956a2d0905d55b7f0ee07ecfb98f7fe2007
Alexander Efremenkov <implimentz@gmail.com>
2016-12-24 12:16:48 +0300

    BinaryPreferencesEditor.java - added base methods for put values

--- library/src/main/java/com/ironz/binaryprefs/BinaryPreferencesEditor.java
+++ library/src/main/java/com/ironz/binaryprefs/BinaryPreferencesEditor.java
@@ -22,41 +22,49 @@ final class BinaryPreferencesEditor implements SharedPreferences.Editor {
 
     @Override
     public SharedPreferences.Editor putString(String key, String value) {
+        commitMap.put(key, value);
         return this;
     }
 
     @Override
     public SharedPreferences.Editor putStringSet(String key, Set<String> values) {
+        commitMap.put(key, values);
         return this;
     }
 
     @Override
     public SharedPreferences.Editor putInt(String key, int value) {
+        commitMap.put(key, value);
         return this;
     }
 
     @Override
     public SharedPreferences.Editor putLong(String key, long value) {
+        commitMap.put(key, value);
         return this;
     }
 
     @Override
     public SharedPreferences.Editor putFloat(String key, float value) {
+        commitMap.put(key, value);
         return this;
     }
 
     @Override
     public SharedPreferences.Editor putBoolean(String key, boolean value) {
+        commitMap.put(key, value);
         return this;
     }
 
     @Override
     public SharedPreferences.Editor remove(String key) {
+        commitMap.remove(key);
         return this;
     }
 
     @Override
     public SharedPreferences.Editor clear() {
+        commitMap.clear();
         return this;
     }
 
