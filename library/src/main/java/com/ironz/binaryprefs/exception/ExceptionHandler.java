40a55b100bcb49f3f87f6655897ed432638d11a5
Alexander <implimentz@gmail.com>
2017-04-13 13:58:18 +0300

    added javadoc

--- library/src/main/java/com/ironz/binaryprefs/exception/ExceptionHandler.java
+++ library/src/main/java/com/ironz/binaryprefs/exception/ExceptionHandler.java
@@ -1,5 +1,13 @@
 package com.ironz.binaryprefs.exception;
 
+/**
+ * Handles errors for implementing custom behaviour
+ */
 public interface ExceptionHandler {
+    /**
+     * Calls while exception are coming (file IO, etc)
+     *
+     * @param e exception
+     */
     void handle(Exception e);
 }
--- library/src/main/java/com/ironz/binaryprefs/exception/FileOperationException.java
+++ library/src/main/java/com/ironz/binaryprefs/exception/FileOperationException.java
@@ -4,7 +4,7 @@ package com.ironz.binaryprefs.exception;
 /**
  * Throws if I/O file operations not finished appropriately
  */
-public class FileOperationException extends RuntimeException {
+public final class FileOperationException extends RuntimeException {
 
     /**
      * @param cause base cause
