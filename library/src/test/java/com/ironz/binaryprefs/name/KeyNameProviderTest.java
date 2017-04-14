322eee0c1aa95ced348bbc70fa3d06c1dd7b7de4
Alexander <implimentz@gmail.com>
2017-04-14 17:56:50 +0300

    created tests for key name provider [2]

--- library/src/test/java/com/ironz/binaryprefs/name/KeyNameProviderTest.java
+++ library/src/test/java/com/ironz/binaryprefs/name/KeyNameProviderTest.java
@@ -6,43 +6,44 @@ import static org.junit.Assert.assertEquals;
 
 public class KeyNameProviderTest {
 
+    private static final String KEY = "key";
     private final KeyNameProvider nameProvider = new KeyNameProvider();
 
     @Test
     public void stringName() {
-        String key = nameProvider.convertStringName("key");
-        assertEquals("key.s", key);
+        String key = nameProvider.convertStringName(KEY);
+        assertEquals(KEY + ".s", key);
     }
 
     @Test
     public void intName() {
-        String key = nameProvider.convertIntName("key");
-        assertEquals("key.i", key);
+        String key = nameProvider.convertIntName(KEY);
+        assertEquals(KEY + ".i", key);
     }
 
     @Test
     public void longName() {
-        String key = nameProvider.convertLongName("key");
-        assertEquals("key.l", key);
+        String key = nameProvider.convertLongName(KEY);
+        assertEquals(KEY + ".l", key);
     }
 
     @Test
     public void booleanName() {
-        String key = nameProvider.convertBooleanName("key");
-        assertEquals("key.b", key);
+        String key = nameProvider.convertBooleanName(KEY);
+        assertEquals(KEY + ".b", key);
     }
 
     @Test
     public void floatName() {
-        String key = nameProvider.convertFloatName("key");
-        assertEquals("key.f", key);
+        String key = nameProvider.convertFloatName(KEY);
+        assertEquals(KEY + ".f", key);
     }
 
     @Test
     public void stringSetName() {
-        String key1 = nameProvider.convertStringSetName("key", 1);
-        String key2 = nameProvider.convertStringSetName("key", 2);
-        assertEquals("key.1.ss", key1);
-        assertEquals("key.2.ss", key2);
+        String key1 = nameProvider.convertStringSetName(KEY, 1);
+        String key2 = nameProvider.convertStringSetName(KEY, 2);
+        assertEquals(KEY + ".1.ss", key1);
+        assertEquals(KEY + ".2.ss", key2);
     }
 }
