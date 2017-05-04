5212a0d203fd88a5f0d86921582c6d26c98c5647
Alexander <implimentz@gmail.com>
2017-05-04 17:57:23 +0300

    added test for checking exception while incorrect decrypt are being

--- library/src/test/java/com/ironz/binaryprefs/encryption/AesByteEncryptionImplTest.java
+++ library/src/test/java/com/ironz/binaryprefs/encryption/AesByteEncryptionImplTest.java
@@ -8,10 +8,12 @@ import static org.junit.Assert.assertEquals;
 public class AesByteEncryptionImplTest {
 
     private AesByteEncryptionImpl aesByteEncryption;
+    private AesByteEncryptionImpl badAesByteEncryption;
 
     @Before
     public void setUp() {
-        aesByteEncryption = new AesByteEncryptionImpl("lzN8kkf7kH816D0U".getBytes(), "ag6pJqG7azJd8qpH".getBytes());
+        aesByteEncryption = new AesByteEncryptionImpl("LZN8KKF7KH816D0U".getBytes(), "AG6PJGG7AZJD8QPH".getBytes());
+        badAesByteEncryption = new AesByteEncryptionImpl("0000000000000000".getBytes(), "0000000000000000".getBytes());
     }
 
     @Test
@@ -21,5 +23,12 @@ public class AesByteEncryptionImplTest {
         byte[] decrypt = aesByteEncryption.decrypt(encrypt);
         String restored = new String(decrypt);
         assertEquals(original, restored);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void badDecrypt() {
+        String original = "some string";
+        byte[] encrypt = aesByteEncryption.encrypt(original.getBytes());
+        badAesByteEncryption.decrypt(encrypt);
     }
 }
