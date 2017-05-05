76699b4c49e81255d01d7ecf78e0deda44d0b3e3
Alexander <implimentz@gmail.com>
2017-05-05 12:05:12 +0300

    improved android directory provider implementation

--- library/src/main/java/com/ironz/binaryprefs/file/AndroidDirectoryProviderImpl.java
+++ library/src/main/java/com/ironz/binaryprefs/file/AndroidDirectoryProviderImpl.java
@@ -5,13 +5,17 @@ import android.content.Context;
 import java.io.File;
 
 
-@SuppressWarnings("unused")
+@SuppressWarnings({"unused", "WeakerAccess"})
 public final class AndroidDirectoryProviderImpl implements DirectoryProvider {
 
     private final File prefs;
 
     public AndroidDirectoryProviderImpl(Context context, String prefName) {
-        File cacheDir = context.getCacheDir();
+        this(context, prefName, false);
+    }
+
+    public AndroidDirectoryProviderImpl(Context context, String prefName, boolean saveInExternal) {
+        File cacheDir = saveInExternal ? context.getExternalCacheDir() : context.getCacheDir();
         prefs = new File(cacheDir, prefName);
         //noinspection ResultOfMethodCallIgnored
         prefs.mkdirs();
