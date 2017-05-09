83a6bccb6f4face4203f8d4d3cdea33ce906bec3
Alexander <implimentz@gmail.com>
2017-05-09 20:05:50 +0300

    fixed preferences manifest declaration

--- library/src/main/java/com/ironz/binaryprefs/dump/DumpReceiver.java
+++ library/src/main/java/com/ironz/binaryprefs/dump/DumpReceiver.java
@@ -3,18 +3,33 @@ package com.ironz.binaryprefs.dump;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
+import com.ironz.binaryprefs.BinaryPreferences;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public final class DumpReceiver extends BroadcastReceiver {
 
     private static final String PREF_NAME = "pref_name";
+    private static final Map<String, BinaryPreferences> BINARY_PREFERENCES_HASH_MAP = new HashMap<>();
 
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        String packageName = context.getPackageName();
-        String directoryPath = intent.getStringExtra(PREF_NAME);
         String prefName = intent.getStringExtra(PREF_NAME);
 
+        if (!BINARY_PREFERENCES_HASH_MAP.containsKey(prefName)) {
+            throw new NullPointerException("Cannot find '%s' preference for dumping!");
+        }
 
+        BinaryPreferences preferences = BINARY_PREFERENCES_HASH_MAP.get(prefName);
+        Map<String, ?> all = preferences.getAll();
+        Log.d(DumpReceiver.class.getName(), all.toString());
+    }
+
+    @SuppressWarnings("unused")
+    public void appendPreference(String name, BinaryPreferences preferences) {
+        BINARY_PREFERENCES_HASH_MAP.put(name, preferences);
     }
 }
