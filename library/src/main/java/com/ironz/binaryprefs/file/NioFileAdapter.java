e5bba53b247a0e1d6b626d82091b65b74b695a68
Alexander <implimentz@gmail.com>
2017-05-09 18:40:24 +0300

    restored tests

--- library/src/main/java/com/ironz/binaryprefs/file/NioFileAdapter.java
+++ library/src/main/java/com/ironz/binaryprefs/file/NioFileAdapter.java
@@ -17,8 +17,6 @@ import java.util.Map;
  */
 public final class NioFileAdapter implements FileAdapter {
 
-    private static final String PREFERENCES_DIR_NAME = "preferences";
-
     final File srcDir;
     private final TaskExecutor taskExecutor;
     private final ByteEncryption encryption;
@@ -42,7 +40,7 @@ public final class NioFileAdapter implements FileAdapter {
 
     @SuppressWarnings("WeakerAccess")
     private NioFileAdapter(File srcDir, TaskExecutor taskExecutor, ByteEncryption encryption) {
-        this.srcDir = new File(srcDir, PREFERENCES_DIR_NAME);
+        this.srcDir = srcDir;
         this.taskExecutor = taskExecutor;
         this.encryption = encryption;
         defineCache();
