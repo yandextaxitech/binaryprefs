c930a6ff4117ef8be3343479db3314826659a87b
Alexander Efremenkov <implimentz@gmail.com>
2017-06-01 21:00:36 +0300

    implemented npe handling

--- library/src/main/java/com/ironz/binaryprefs/serialization/BinaryPrefsObjectOutputImpl.java
+++ library/src/main/java/com/ironz/binaryprefs/serialization/BinaryPrefsObjectOutputImpl.java
@@ -15,15 +15,18 @@ public final class BinaryPrefsObjectOutputImpl implements ObjectOutput {
     private byte[] buffer = new byte[GROW_ARRAY_CAPACITY];
 
     @Override
-    public void writeObject(Object obj) throws IOException {
-        if (!(obj instanceof Externalizable)) {
+    public void writeObject(Object value) throws IOException {
+        if (value == null) {
+            throw new NullPointerException("Can't serialize null object");
+        }
+        if (!(value instanceof Externalizable)) {
             throw new UnsupportedOperationException(
                     String.format("Can't serialize object '%s' which are not implements '%s' interface",
-                            obj.getClass().getSimpleName(),
+                            value.getClass().getSimpleName(),
                             Externalizable.class.getName())
             );
         }
-        Externalizable externalizable = (Externalizable) obj;
+        Externalizable externalizable = (Externalizable) value;
         externalizable.writeExternal(this);
     }
 
@@ -89,6 +92,9 @@ public final class BinaryPrefsObjectOutputImpl implements ObjectOutput {
 
     @Override
     public void writeBytes(String value) throws IOException {
+        if (value == null) {
+            throw new NullPointerException("Can't serialize null object");
+        }
         byte[] trim = value.getBytes();
         for (byte b : trim) {
             byte[] bytes = Bits.byteToBytesWithFlag(b);
@@ -98,6 +104,9 @@ public final class BinaryPrefsObjectOutputImpl implements ObjectOutput {
 
     @Override
     public void writeChars(String value) throws IOException {
+        if (value == null) {
+            throw new NullPointerException("Can't serialize null object");
+        }
         char[] trim = value.toCharArray();
         for (char c : trim) {
             byte[] bytes = Bits.charToBytesWithFlag(c);
@@ -107,6 +116,9 @@ public final class BinaryPrefsObjectOutputImpl implements ObjectOutput {
 
     @Override
     public void writeUTF(String value) throws IOException {
+        if (value == null) {
+            throw new NullPointerException("Can't serialize null object");
+        }
         byte[] bytes = value.getBytes("UTF-8");
         write(bytes, 0, bytes.length);
     }
