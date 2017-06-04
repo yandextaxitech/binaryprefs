374d73ce1b76ec6863cf126d475b93ca2ebe42c2
Alexander Efremenkov <implimentz@gmail.com>
2017-06-04 16:50:07 +0300

    implemented closed output

--- library/src/main/java/com/ironz/binaryprefs/serialization/BinaryPrefsObjectInputImpl.java
+++ library/src/main/java/com/ironz/binaryprefs/serialization/BinaryPrefsObjectInputImpl.java
@@ -8,8 +8,13 @@ public final class BinaryPrefsObjectInputImpl implements ObjectInput {
 
     private byte[] buffer;
     private int offset = 0;
+    private boolean closed = false;
 
     public <T extends Externalizable> T deserialize(byte[] bytes, Class<? extends T> clazz) throws Exception {
+
+        checkNull(clazz);
+        checkClosed();
+
         T instance = clazz.newInstance();
         instance.readExternal(this);
         return instance;
@@ -22,37 +27,43 @@ public final class BinaryPrefsObjectInputImpl implements ObjectInput {
 
     @Override
     public int read() throws IOException {
+        checkClosed();
         checkBounds();
         return 0;
     }
 
     @Override
     public int read(byte[] b) throws IOException {
+        checkClosed();
         checkBounds();
         return 0;
     }
 
     @Override
     public int read(byte[] b, int off, int len) throws IOException {
+        checkClosed();
         checkBounds();
         return 0;
     }
 
     @Override
     public long skip(long n) throws IOException {
+        checkClosed();
         checkBounds();
         return 0;
     }
 
     @Override
     public int available() throws IOException {
+        checkClosed();
+
         checkBounds();
         return 0;
     }
 
     @Override
     public void close() throws IOException {
-
+        closed = true;
     }
 
     @Override
@@ -72,79 +83,103 @@ public final class BinaryPrefsObjectInputImpl implements ObjectInput {
 
     @Override
     public boolean readBoolean() throws IOException {
+        checkClosed();
         checkBounds();
         return false;
     }
 
     @Override
     public byte readByte() throws IOException {
+        checkClosed();
         checkBounds();
         return 0;
     }
 
     @Override
     public int readUnsignedByte() throws IOException {
+        checkClosed();
         checkBounds();
         return 0;
     }
 
     @Override
     public short readShort() throws IOException {
+        checkClosed();
         checkBounds();
         return 0;
     }
 
     @Override
     public int readUnsignedShort() throws IOException {
+        checkClosed();
         checkBounds();
         return 0;
     }
 
     @Override
     public char readChar() throws IOException {
+        checkClosed();
         checkBounds();
         return 0;
     }
 
     @Override
     public int readInt() throws IOException {
+        checkClosed();
         checkBounds();
         return 0;
     }
 
     @Override
     public long readLong() throws IOException {
+        checkClosed();
         checkBounds();
         return 0;
     }
 
     @Override
     public float readFloat() throws IOException {
+        checkClosed();
         checkBounds();
         return 0;
     }
 
     @Override
     public double readDouble() throws IOException {
+        checkClosed();
         checkBounds();
         return 0;
     }
 
     @Override
     public String readLine() throws IOException {
+        checkClosed();
         checkBounds();
         return null;
     }
 
     @Override
     public String readUTF() throws IOException {
+        checkClosed();
         checkBounds();
         return null;
+    }
+
+    private void checkClosed() throws IOException {
+        if (closed) {
+            throw new IOException("Cannot write to already closed object output");
+        }
     }
 
     private void checkBounds() {
         if (offset >= buffer.length - 1) {
             throw new ArrayIndexOutOfBoundsException("Can't read out of bounds array");
+        }
+    }
+
+    private void checkNull(Object value) {
+        if (value == null) {
+            throw new NullPointerException("Can't serialize null object");
         }
     }
 }
