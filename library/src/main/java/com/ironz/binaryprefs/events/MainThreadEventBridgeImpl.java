dc75e8f17c59b1fa694015b029f7a6c402c1f2c0
Alexander <implimentz@gmail.com>
2017-06-08 13:55:27 +0300

    implemented main thread event bridge listener

---
+++ library/src/main/java/com/ironz/binaryprefs/events/MainThreadEventBridgeImpl.java
@@ -0,0 +1,57 @@
+package com.ironz.binaryprefs.events;
+
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.os.Handler;
+import com.ironz.binaryprefs.Preferences;
+import com.ironz.binaryprefs.cache.CacheProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Simple preference change listener bridge
+ */
+@SuppressWarnings("unused")
+public final class MainThreadEventBridgeImpl implements EventBridge {
+
+    private final List<OnSharedPreferenceChangeListener> listeners = new ArrayList<>();
+    private final CacheProvider cacheProvider;
+    private final Handler handler = new Handler();
+
+    public MainThreadEventBridgeImpl(CacheProvider cacheProvider) {
+        this.cacheProvider = cacheProvider;
+    }
+
+    @Override
+    public void registerOnSharedPreferenceChangeListener(OnSharedPreferenceChangeListener listener) {
+        listeners.add(listener);
+    }
+
+    @Override
+    public void unregisterOnSharedPreferenceChangeListener(OnSharedPreferenceChangeListener listener) {
+        listeners.remove(listener);
+    }
+
+    @Override
+    public void notifyListenersUpdate(Preferences preferences, String key, byte[] value) {
+        cacheProvider.put(key, value);
+        notifyListeners(preferences, key);
+    }
+
+    @Override
+    public void notifyListenersRemove(Preferences preferences, String key) {
+        cacheProvider.remove(key);
+        notifyListeners(preferences, key);
+    }
+
+    private void notifyListeners(final Preferences preferences, final String key) {
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                for (OnSharedPreferenceChangeListener listener : listeners) {
+                    listener.onSharedPreferenceChanged(preferences, key);
+                }
+            }
+        });
+    }
+}
