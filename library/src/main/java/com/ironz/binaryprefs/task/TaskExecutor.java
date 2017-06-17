3046aef8b232fc89de4aa14d6088ab8d5491f1ca
Alexander Efremenkov <implimentz@gmail.com>
2017-06-17 14:12:29 +0300

    javadoc changes

--- library/src/main/java/com/ironz/binaryprefs/task/TaskExecutor.java
+++ library/src/main/java/com/ironz/binaryprefs/task/TaskExecutor.java
@@ -1,11 +1,12 @@
 package com.ironz.binaryprefs.task;
 
 /**
- * Abstraction for task running
+ * Abstraction for task running. You should guarantee sequential task execution.
  */
 public interface TaskExecutor {
     /**
-     * After submitting executor adds this task in queue and runs later
+     * After submitting executor adds this task in queue and runs later.
+     * Tasks guaranteed to execute sequentially.
      *
      * @param runnable instance for task execution
      */
