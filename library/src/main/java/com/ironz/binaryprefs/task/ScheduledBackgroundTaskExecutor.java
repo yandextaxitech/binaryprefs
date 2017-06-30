c66e4e0fceca4595642ccd00eab5f849a5cb1a6d
Alexander Efremenkov <implimentz@gmail.com>
2017-06-30 23:11:39 +0300

    removed thread factory creating

--- library/src/main/java/com/ironz/binaryprefs/task/ScheduledBackgroundTaskExecutor.java
+++ library/src/main/java/com/ironz/binaryprefs/task/ScheduledBackgroundTaskExecutor.java
@@ -5,7 +5,6 @@ import com.ironz.binaryprefs.exception.ExceptionHandler;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.concurrent.ThreadFactory;
 
 /**
  * Performs all submitted tasks in one separated thread sequentially.
@@ -15,16 +14,7 @@ public final class ScheduledBackgroundTaskExecutor implements TaskExecutor {
 
     private final ExceptionHandler exceptionHandler;
 
-    private static final ExecutorService executor = Executors.newSingleThreadExecutor(new ThreadFactory() {
-        @Override
-        public Thread newThread(Runnable r) {
-            Thread thread = new Thread();
-            thread.setName(ScheduledBackgroundTaskExecutor.class.getSimpleName() + "-thread");
-            thread.setDaemon(true);
-            thread.setPriority(Thread.MAX_PRIORITY);
-            return thread;
-        }
-    });
+    private static final ExecutorService executor = Executors.newSingleThreadExecutor();
 
     public ScheduledBackgroundTaskExecutor(ExceptionHandler exceptionHandler) {
         this.exceptionHandler = exceptionHandler;
