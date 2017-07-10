d2687c9acf988deefa083db928eb1d673a57ced7
Alexander <implimentz@gmail.com>
2017-07-10 16:30:00 +0300

    added test for checking one type with two different keys

--- library/src/test/java/com/ironz/binaryprefs/serialization/serializer/persistable/PersistableRegistryTest.java
+++ library/src/test/java/com/ironz/binaryprefs/serialization/serializer/persistable/PersistableRegistryTest.java
@@ -29,8 +29,28 @@ public class PersistableRegistryTest {
 
     @Test
     public void register() {
-        registry.register(TestUser.KEY, TestUser.class);
-        Class<? extends Persistable> clazz = registry.get(TestUser.KEY);
-        assertEquals(TestUser.class, clazz);
+        String key = TestUser.KEY;
+        Class<TestUser> clazz = TestUser.class;
+
+        registry.register(key, clazz);
+        Class<? extends Persistable> getType = registry.get(key);
+
+        assertEquals(clazz, getType);
+    }
+
+    @Test
+    public void registerTypeWithTwoKeys() {
+        String key = TestUser.KEY;
+        String key2 = key + "2";
+        Class<TestUser> type = TestUser.class;
+
+        registry.register(key, type);
+        registry.register(key2, type);
+
+        Class<? extends Persistable> getType = registry.get(key);
+        Class<? extends Persistable> getType2 = registry.get(key2);
+
+        assertEquals(type, getType);
+        assertEquals(type, getType2);
     }
 }
