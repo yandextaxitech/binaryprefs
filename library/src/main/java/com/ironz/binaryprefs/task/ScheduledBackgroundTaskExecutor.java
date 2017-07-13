b7eed2278d590ac6024fd9faf3219b450a300372
Alexander <implimentz@gmail.com>
2017-07-13 09:57:29 +0300

    added thread factory for task executor

--- library/src/main/java/com/ironz/binaryprefs/task/ScheduledBackgroundTaskExecutor.java
+++ library/src/main/java/com/ironz/binaryprefs/task/ScheduledBackgroundTaskExecutor.java
@@ -3,10 +3,7 @@ package com.ironz.binaryprefs.task;
 import com.ironz.binaryprefs.exception.ExceptionHandler;
 
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 /**
  * Performs all submitted tasks in one separated thread sequentially.
@@ -25,13 +22,29 @@ public final class ScheduledBackgroundTaskExecutor implements TaskExecutor {
         executor = initExecutor(prefName);
     }
 
-    private ExecutorService initExecutor(String prefName) {
+    private ExecutorService initExecutor(final String prefName) {
         if (executorsMap.containsKey(prefName)) {
             return executorsMap.get(prefName);
         }
-        ExecutorService service = Executors.newSingleThreadExecutor();
+        ExecutorService service = Executors.newFixedThreadPool(1, createThreadFactory(prefName));
         executorsMap.put(prefName, service);
         return service;
+    }
+
+    private ThreadFactory createThreadFactory(final String prefName) {
+        return new ThreadFactory() {
+            @Override
+            public Thread newThread(Runnable r) {
+                return createThread(r, prefName);
+            }
+        };
+    }
+
+    private Thread createThread(Runnable r, String prefName) {
+        Thread thread = new Thread(r);
+        thread.setName("binaryprefs-pool-" + prefName);
+        thread.setPriority(Thread.MAX_PRIORITY);
+        return thread;
     }
 
     @Override
