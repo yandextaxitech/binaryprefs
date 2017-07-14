4af6ede8784f58621b5f437b278b1ef020f90342
Alexander <implimentz@gmail.com>
2017-07-14 11:43:26 +0300

    added javadoc for builder and also added TODO's

--- library/src/main/java/com/ironz/binaryprefs/BinaryPreferencesBuilder.java
+++ library/src/main/java/com/ironz/binaryprefs/BinaryPreferencesBuilder.java
@@ -24,10 +24,17 @@ import com.ironz.binaryprefs.serialization.serializer.persistable.PersistableReg
 import com.ironz.binaryprefs.task.ScheduledBackgroundTaskExecutor;
 import com.ironz.binaryprefs.task.TaskExecutor;
 
+/**
+ * Describes public api for building preferences instance.
+ */
 @SuppressWarnings("unused")
 public final class BinaryPreferencesBuilder {
 
-    private static final String DEFAULT_NAME = "default";
+    /**
+     * Default name of preferences which name has not been defined.
+     */
+    @SuppressWarnings("WeakerAccess")
+    public static final String DEFAULT_NAME = "default";
 
     private final Context context;
     private final PersistableRegistry persistableRegistry = new PersistableRegistry();
@@ -38,40 +45,105 @@ public final class BinaryPreferencesBuilder {
     private ByteEncryption byteEncryption = ByteEncryption.NO_OP;
     private ExceptionHandler exceptionHandler = ExceptionHandler.PRINT;
 
+    /**
+     * Creates builder with base parameters.
+     * <p>
+     * Note: Please, use only one instance of preferences by name,
+     * this saves you from non-reasoned allocations.
+     * </p>
+     *
+     * @param context target context. Using application context is
+     *                very much appreciated
+     */
     public BinaryPreferencesBuilder(Context context) {
         this.context = context;
     }
 
+    /**
+     * Defines preferences name for build instance.
+     *
+     * @param name target preferences name. Default name is {@link #DEFAULT_NAME}
+     * @return current builder instance
+     */
     public BinaryPreferencesBuilder name(String name) {
         this.name = name;
         return this;
     }
 
+    /**
+     * Defines usage of external directory for preferences saving.
+     * Default value is {@code false}.
+     *
+     * @param value {@code true} if would use external storage, {@code false} otherwise
+     * @return current builder instance
+     */
     public BinaryPreferencesBuilder externalStorage(boolean value) {
         this.externalStorage = value;
         return this;
     }
 
-    public BinaryPreferencesBuilder supportInterProcess() {
-        this.supportInterProcess = true;
+    /**
+     * Defines usage of IPC mechanism for delivering key updates and cache consistency.
+     * Default value is {@code false}.
+     * <p>
+     * Note: Please, note that one key change delta should be less than 1 (one) megabyte
+     * because IPC data transferring is limited by this capacity.
+     * </p>
+     *
+     * @param value {@code true} if would use IPC, {@code false} otherwise
+     * @return current builder instance
+     */
+    public BinaryPreferencesBuilder supportInterProcess(boolean value) {
+        this.supportInterProcess = value;
         return this;
     }
 
+    /**
+     * Defines encryption implementation which performs vice-versa byte encryption operations.
+     * Default value is {@link ByteEncryption#NO_OP}
+     *
+     * @param byteEncryption byte encryption implementation
+     * @return current builder instance
+     */
     public BinaryPreferencesBuilder encryption(ByteEncryption byteEncryption) {
         this.byteEncryption = byteEncryption;
         return this;
     }
 
+    /**
+     * Defines exception handler implementation which handles exception events e.g. logging operations.
+     * Default value is {@link ExceptionHandler#PRINT}
+     *
+     * @param exceptionHandler exception handler implementation
+     * @return current builder instance
+     */
     public BinaryPreferencesBuilder exceptionHandler(ExceptionHandler exceptionHandler) {
         this.exceptionHandler = exceptionHandler;
         return this;
     }
 
+    /**
+     * Registers {@link Persistable} data-object for de/serialization process.
+     * All {@link Persistable} data-objects should be registered for understanding
+     * de/serialization contract during cache initialization.
+     *
+     * @param key         target key which uses for fetching {@link Persistable}
+     *                    in {@link PreferencesEditor#putPersistable(String, Persistable)} method
+     * @param persistable target class type which implements {@link Persistable} interface
+     * @return current builder instance
+     */
     public BinaryPreferencesBuilder registerPersistable(String key, Class<? extends Persistable> persistable) {
         persistableRegistry.register(key, persistable);
         return this;
     }
 
+    /**
+     * Builds preferences instance with predefined or default parameters.
+     * This method will fails if invocation performed not in the main thread.
+     *
+     * @return preferences instance with predefined or default parameters.
+     * @see PreferencesInitializationException
+     */
     public Preferences build() {
 
         if (Looper.myLooper() != Looper.getMainLooper()) {
@@ -106,6 +178,7 @@ public final class BinaryPreferencesBuilder {
                 lockFactory
         );
 
+        // TODO: 7/14/17 redesign this workaround in future
         if (eventsBridge instanceof BroadcastEventBridgeImpl) { //shit happens
             ((BroadcastEventBridgeImpl) eventsBridge).definePreferences(preferences);
         }
