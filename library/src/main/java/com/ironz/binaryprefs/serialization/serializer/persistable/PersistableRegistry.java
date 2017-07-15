c917c1858abc4cec231151ceae9b953748927bf4
Alexander Efremenkov <implimentz@gmail.com>
2017-07-15 11:58:25 +0300

    small persistable registry exception message changes

--- library/src/main/java/com/ironz/binaryprefs/serialization/serializer/persistable/PersistableRegistry.java
+++ library/src/main/java/com/ironz/binaryprefs/serialization/serializer/persistable/PersistableRegistry.java
@@ -7,7 +7,8 @@ public final class PersistableRegistry {
 
     private static final String CANNOT_FIND_MESSAGE = "Cannot find Persistable type for '%s' key. " +
             "Please, add it through 'registerPersistable' builder method.";
-    private static final String ALREADY_REGISTERED_MESSAGE = "Registry already contains '%s' class for '%s' key.";
+    private static final String ALREADY_REGISTERED_MESSAGE = "Registry already contains '%s' class for '%s' key. " +
+            "Please, don't add persistable by similar key twice.";
 
     private final Map<String, Class<? extends Persistable>> map = new HashMap<>();
 
