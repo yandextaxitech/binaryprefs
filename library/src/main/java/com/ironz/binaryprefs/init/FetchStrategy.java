5765d4bd9547d6c73209b3c6cee9f9dddaac7396
Alexander Efremenkov <implimentz@gmail.com>
2017-09-22 14:15:56 +0300

    added javadoc for fetching strategy

--- library/src/main/java/com/ironz/binaryprefs/init/FetchStrategy.java
+++ library/src/main/java/com/ironz/binaryprefs/init/FetchStrategy.java
@@ -1,5 +1,15 @@
 package com.ironz.binaryprefs.init;
 
+/**
+ * Interface which describes contract for object fetching
+ */
 public interface FetchStrategy {
+    /**
+     * Returns value for target key
+     *
+     * @param key      given key for fetching
+     * @param defValue default value if target value does not exists
+     * @return actual value if exists, default value otherwise
+     */
     Object getValue(String key, Object defValue);
 }
