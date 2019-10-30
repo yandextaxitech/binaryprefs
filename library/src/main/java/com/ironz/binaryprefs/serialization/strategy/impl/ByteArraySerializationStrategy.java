11b1f442bb546f2fea91eaa465c839dcc9b81841
Andrey Chernoprudov <dinloq@gmail.com>
2019-10-30 14:56:17 +0500

    Copy cached byte arrays values to prevent outer modifications

--- library/src/main/java/com/ironz/binaryprefs/serialization/strategy/impl/ByteArraySerializationStrategy.java
+++ library/src/main/java/com/ironz/binaryprefs/serialization/strategy/impl/ByteArraySerializationStrategy.java
@@ -4,13 +4,15 @@ import com.ironz.binaryprefs.serialization.SerializerFactory;
 import com.ironz.binaryprefs.serialization.serializer.ByteArraySerializer;
 import com.ironz.binaryprefs.serialization.strategy.SerializationStrategy;
 
+import java.util.Arrays;
+
 public final class ByteArraySerializationStrategy implements SerializationStrategy {
 
     private final byte[] value;
     private final ByteArraySerializer byteArraySerializer;
 
     public ByteArraySerializationStrategy(byte[] value, SerializerFactory serializerFactory) {
-        this.value = value;
+        this.value = Arrays.copyOf(value, value.length);
         this.byteArraySerializer = serializerFactory.getByteArraySerializer();
     }
 
